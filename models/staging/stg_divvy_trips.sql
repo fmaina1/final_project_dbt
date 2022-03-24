c5a49e1ce93ff7db865c96727998d593571d15cd
Michael Shoemaker <michaelshoemaker50@gmail.com>
2022-03-24 23:05:40 +0000

    Fixed DateTime to Day Macros

--- models/staging/stg_divvy_trips.sql
+++ models/staging/stg_divvy_trips.sql
@@ -6,8 +6,8 @@ SELECT ride_id
 ,ended_at
 ,start_station_name
 ,end_station_name
-,{{ get_day_of_week(started_at) }} as start_day
-,{{ get_day_of_week(ended_at) }} as end_day
+,{{ start_date_to_day(started_at) }} as start_day
+,{{ end_date_to_day(ended_at) }} as end_day
 FROM {{ source('staging','external_divvy_data')}}
 WHERE start_station_name is not null
 and end_station_name is not null

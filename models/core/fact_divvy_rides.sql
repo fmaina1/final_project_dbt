c762e36650d94ffcee54e6e2e70bcee91132a6bc
Michael Shoemaker <michaelshoemaker50@gmail.com>
2022-03-26 18:05:00 +0000

    Added Core Models and Documentation

---
+++ models/core/fact_divvy_rides.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table') }}
+
+with dupe_recs as(
+    SELECT *
+    ,row_number()over(partition by ride_id order by start_station_name) as RowNbr
+    FROM {{ ref('stg_divvy_trips') }}
+)
+
+
+SELECT ride_id
+,rideable_type
+,member_casual
+,started_at
+,ended_at
+,start_station_name
+,end_station_name
+,start_day
+,end_day
+FROM dupe_recs
+WHERE RowNbr = 1
